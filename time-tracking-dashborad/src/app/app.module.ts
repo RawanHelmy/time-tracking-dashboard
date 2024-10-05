f66f8ea6bbdc608b24fb7d8914ec3c574399e473
rawanhelmy <rawanhelmy98@gmail.com>
2024-10-05 14:53:52 +0300

    components added , based styling

--- time-tracking-dashborad/src/app/app.module.ts
+++ time-tracking-dashborad/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ReportCardComponent } from './components/report-card/report-card.component';
+import { CardsContainerComponent } from './components/cards-container/cards-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    ReportCardComponent,
+    CardsContainerComponent
   ],
   imports: [
     BrowserModule,
